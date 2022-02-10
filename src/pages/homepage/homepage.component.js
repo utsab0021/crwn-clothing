97011f2be3af5bc8ae4bc7167ab164a88cb64ffe
utsab0021 <utsabdas211@gmail.com>
2022-02-10 21:12:54 +0530

    broken our code into smaller component,finish styling of our menu-item

---
+++ src/pages/homepage/homepage.component.js
@@ -0,0 +1,9 @@
+import './homepage.styles.scss';
+import React from "react";
+import Directory from '../../components/directory/directory.component';
+
+export const HomePage = () => (
+  <div className="homepage">
+    <Directory/>
+  </div>
+);
